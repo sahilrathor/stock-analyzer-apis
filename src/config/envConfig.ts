b9d009ad4f9116d78349b4f5815b82648211f478
Sahil Rathor <sr7015342@gmail.com>
2025-06-29 15:59:52 +0530

    .

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const envConfig: EnvInterface = {
-    PORT: Number(process.env.PORT),
-    JWT_SECRET: process.env.JWT_SECRET || "secret",
+    PORT: Number(process.env.PORT) || 5000,
+    JWT_SECRET: process.env.JWT_SECRET || "@mySecret",
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || "",
     TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID || "",
     UPSTOX_API_KEY: process.env.UPSTOX_API_KEY || "",
