a4b50aa879c38ed1b67139fd92a58ec1ae17be1e
Sahil Rathor <sr7015342@gmail.com>
2025-07-09 00:47:18 +0530

    stocks services response updated

--- src/services/stocks/stocks.ts
+++ src/services/stocks/stocks.ts
@@ -7,27 +7,65 @@ class StockService {
         try {
             const result = await pool.query("SELECT id, name, symbol, exchange, sector, industry FROM stocks");
             return res.status(200).json({
-                message: "Stocks fetched successfully",
-                stocksCount: result.rowCount,
-                stocks: result.rows,
+                meta: {
+                    message: "Stocks fetched successfully",
+                    error: false,
+                },
+                data: {
+                    stocksCount: result.rowCount,
+                    stocks: result.rows,
+                },
             });
         } catch (error: any) {
             console.log("searchStock error:", error);
-            return res.status(500).json({ message: "Internal Server Error" });
+            return res.status(500).json({
+                meta: {
+                    message: "Internal Server Error",
+                    error: true,
+                },
+            });
         }
     }
 
     static async searchStock(req: Request, res: Response) {
         const { query } = req.query;
         if (!query) {
-            return res.status(400).json({ message: "Missing query" });
+            return res.status(400).json({
+                meta: {
+                    message: "Missing query",
+                    error: true,
+                },
+            });
         }
         try {
             const result = await yFinance.searchSymbol(query as string);
-            return res.status(200).json({ result: result });
+
+            const stocks = result.quotes;
+            const news = result.news;
+            return res.status(200).json({
+                meta: {
+                    message: "Symbol searched successfully",
+                    error: false,
+                },
+                data: {
+                    stocks: {
+                        count: stocks.length,
+                        stocks: stocks,
+                    },
+                    news: {
+                        count: news.length,
+                        news: news,
+                    },
+                },
+            });
         } catch (error: any) {
             console.log("searchStock error:", error);
-            return res.status(500).json({ message: "Internal Server Error" });
+            return res.status(500).json({
+                meta: {
+                    message: "Internal Server Error",
+                    error: true,
+                },
+            });
         }
     }
 
@@ -35,12 +73,22 @@ class StockService {
         const { name, symbol, exchange, sector, industry } = req.body;
 
         if (!name || !symbol) {
-            return res.status(400).json({ message: "Missing field" });
+            return res.status(400).json({
+                meta: {
+                    message: "Missing field",
+                    error: true,
+                },
+            });
         }
 
         const isExists = await pool.query("SELECT id FROM stocks WHERE symbol = $1", [symbol]);
         if (isExists.rowCount !== 0) {
-            return res.status(409).json({ message: "Stock already exists with this symbol" });
+            return res.status(409).json({
+                meta: {
+                    message: "Stock already exists with this symbol",
+                    error: true,
+                },
+            });
         }
 
         try {
@@ -50,11 +98,19 @@ class StockService {
             );
 
             return res.status(201).json({
-                message: "Stock added successfully",
+                meta: {
+                    message: "Stock added successfully",
+                    error: false,
+                },
             });
         } catch (error: any) {
             console.log("addStock error:", error);
-            return res.status(500).json({ message: "Internal Server Error" });
+            return res.status(500).json({
+                meta: {
+                    message: "Internal Server Error",
+                    error: true,
+                },
+            });
         }
     }
 
@@ -62,22 +118,42 @@ class StockService {
         const { id } = req.params;
 
         if (!id) {
-            return res.status(400).json({ message: "missing field: id" });
+            return res.status(400).json({
+                meta: {
+                    message: "missing field: id",
+                    error: true,
+                },
+            });
         }
 
         const isExists = await pool.query("SELECT id FROM stocks WHERE id = $1", [id]);
 
         if (isExists.rowCount === 0) {
-            return res.status(400).json({ message: "Stock not found" });
+            return res.status(400).json({
+                meta: {
+                    message: "Stock not found",
+                    error: true,
+                },
+            });
         }
 
         try {
             await pool.query("DELETE FROM stocks WHERE id = $1", [id]);
 
-            return res.status(200).json({ message: "Stock deletd successhully" });
+            return res.status(200).json({
+                meta: {
+                    message: "Stock deletd successhully",
+                    error: false,
+                },
+            });
         } catch (error) {
             console.log("removeStock error:", error);
-            return res.status(200).json({ message: "Internal Server Error" });
+            return res.status(200).json({
+                meta: {
+                    message: "Internal Server Error",
+                    error: true,
+                },
+            });
         }
     }
 
@@ -85,7 +161,12 @@ class StockService {
         const { id } = req.params;
 
         if (!id) {
-            return res.status(400).json({ message: "missing field: id" });
+            return res.status(400).json({   
+                meta: {
+                    message: "missing field: id",
+                    error: true,
+                },
+            });
         }
 
         const result = await pool.query("SELECT symbol FROM stocks WHERE id = $1", [id]);
@@ -95,10 +176,23 @@ class StockService {
         try {
             const result = await yFinance.quoteSymbol(symbol as string);
             // console.log('yfin:', result)
-            return res.status(200).json({ result: result });
+            return res.status(200).json({
+                meta: {
+                    message: "Quote info fetched successfully",
+                    error: false,
+                },
+                data: {
+                    result: result,
+                },
+            });
         } catch (error) {
             console.log("getQuoteInfo error:", error);
-            return res.status(200).json({ message: "Internal Server Error" });
+            return res.status(200).json({
+                meta: {
+                    message: "Internal Server Error",
+                    error: true,
+                },
+            });
         }
     }
 }
